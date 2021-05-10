4c7c970538f0c33ea93c9f789494b852fd00bcfb
Ilia Biltuev <i.biltuev@redmadrobot.com>
2021-05-11 00:20:22 +0300

    added gesture recognizer + added kicking ball impulse

--- 3DBreakout/GameViewController.swift
+++ 3DBreakout/GameViewController.swift
@@ -19,31 +19,14 @@ import SceneKit
  */
 class GameViewController: UIViewController {
     
+    // MARK: - Properties
+    
     var scnView: SCNView!
     var scnScene: SCNScene!
     var game = GameHelper.sharedInstance
+    var ball: SCNNode!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    
-        setupScene()
-        setupNodes()
-        setupSounds()
-    }
-    
-    func setupScene() {
-        scnView = (self.view as! SCNView)
-        scnView.delegate = self
-        scnScene = SCNScene(named: "Breaker.scnassets/Scenes/Game.scn")
-        scnView.scene = scnScene
-    }
-    
-    func setupNodes() {
-        scnScene.rootNode.addChildNode(game.hudNode)
-    }
-    
-    func setupSounds() {
-    }
+    // MARK: - Override
     
     override var prefersStatusBarHidden: Bool {
         return true
@@ -52,7 +35,57 @@ class GameViewController: UIViewController {
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
         return .allButUpsideDown
     }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        setupScene()
+        setupNodes()
+        setupGestures()
+    }
+    
+    // MARK: - UIGestureRecognizer
+    
+    @objc func sceneViewDidTap(recognizer: UIGestureRecognizer) {
+        let location = recognizer.location(in: scnView)
+        let hitResults = scnView.hitTest(location, options: nil)
+        guard
+            let tappedNode = hitResults.first?.node,
+            tappedNode.name == "ball"
+        else {
+            return
+        }
+        kickBall()
+    }
+    
+    // MARK: - Private
+    
+    private func setupScene() {
+        scnView = (self.view as! SCNView)
+        scnView.delegate = self
+        scnScene = SCNScene(named: "Breaker.scnassets/Scenes/Game.scn")
+        scnView.scene = scnScene
+    }
+    
+    private func setupNodes() {
+        ball = scnScene.rootNode.childNode(withName: "ball", recursively: true)!
+    }
+    
+    private func setupGestures() {
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(sceneViewDidTap(recognizer:)))
+        scnView.addGestureRecognizer(tapGestureRecognizer)
+    }
+    
+    private func kickBall() {
+        // TODO: Использовать далее вот этот метод - приложить силу в конкретную точку
+        //ball.physicsBody?.applyForce(T##direction: SCNVector3##SCNVector3, at: T##SCNVector3, asImpulse: T##Bool)
+        
+        let direction = SCNVector3(0, 3, -5)
+        ball.physicsBody?.applyForce(direction, asImpulse: true)
+    }
 }
+
+// MARK: - SCNSceneRendererDelegate
 
 extension GameViewController: SCNSceneRendererDelegate {
     func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
